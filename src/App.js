fdc3f0b47c04b64dd29961d81201c08f15520ad7
Andrew Bustos <infernious@live.com>
2024-01-20 15:52:01 -0800

    initial routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,99 @@
-import "./App.css"
+import { Button, Stack, useDisclosure, Link, Text } from "@chakra-ui/react"
+import { motion } from "framer-motion"
+import { useState } from "react"
+import { Outlet } from "react-router-dom"
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons"
 
-function App() {
-	return <div className='App'></div>
+export default function App() {
+	const { getButtonProps, getDisclosureProps, isOpen, onToggle } =
+		useDisclosure({ defaultIsOpen: true })
+	const [hidden, setHidden] = useState(!isOpen)
+
+	const clicker = () => {}
+
+	const handleToggle = () => {
+		clicker() // Execute the clicker function
+		onToggle() // Toggle the disclosure state
+	}
+
+	return (
+		<div className='flex items-start'>
+			<motion.div
+				{...getDisclosureProps()}
+				hidden={hidden}
+				initial={{ width: isOpen ? 200 : 0 }}
+				onAnimationStart={() => setHidden(false)}
+				onAnimationComplete={() => setHidden(!isOpen)}
+				animate={{ width: isOpen ? 200 : 0 }}
+				style={{
+					overflow: "hidden",
+					whiteSpace: "nowrap",
+					height: "100vh",
+					zIndex: 2,
+				}}
+				className='bg-[#f7fbf2] shadow-lg shadow-slate-900/20 shadow-b-2 shadow-r-[3px] shadow-[#f7fbf2]]'
+			>
+				<Stack mt={3} gap={5}>
+					<Button
+						bg='#34693F'
+						color='white'
+						ml={2}
+						w={10}
+						_hover={{ bg: "#6fab7b" }}
+						{...getButtonProps()}
+					>
+						<CloseIcon />
+					</Button>
+					<Link
+						href='/'
+						bg='#34693F'
+						color='white'
+						borderRightRadius={20}
+						py={1}
+						px={5}
+						w={36}
+						textAlign='right'
+					>
+						<Text fontWeight='bold' fontSize={20}>
+							Dashboard
+						</Text>
+					</Link>
+					<Link
+						href='/upload'
+						bg='#34693F'
+						color='white'
+						borderRightRadius={20}
+						py={1}
+						px={5}
+						w={36}
+						textAlign='right'
+					>
+						<Text fontWeight='bold' fontSize={20}>
+							Upload
+						</Text>
+					</Link>
+				</Stack>
+			</motion.div>
+			<Button
+				style={{
+					position: "absolute",
+					top: 0,
+					left: 0,
+					zIndex: 0,
+				}}
+				{...getButtonProps()}
+				onClick={handleToggle}
+			>
+				<HamburgerIcon />
+			</Button>
+			<div
+				style={{
+					marginLeft: `${!isOpen ? "3.5rem" : "1rem"}`,
+					transition: "margin-left 0.1s ease",
+				}}
+			>
+				<Outlet />
+			</div>
+		</div>
+	)
 }
-
-export default App
--- src/index.js
+++ src/index.js
@@ -3,12 +3,41 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import { ChakraProvider } from "@chakra-ui/react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { Dashboard } from "./routes/dashboard"
+import { Upload } from "./routes/upload"
+import { Results } from "./routes/results"
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Dashboard />,
+			},
+			{
+				path: "/upload",
+				element: <Upload />,
+			},
+			{
+				path: "/results",
+				element: <Results />,
+			},
+			{
+				path: "*",
+				element: <div>404</div>,
+			},
+		],
+	},
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
 	<ChakraProvider>
 		<React.StrictMode>
-			<App />
+			<RouterProvider router={router} />
 		</React.StrictMode>
 	</ChakraProvider>
 )
